070d8472520b28557f1e580f03f8bb2701967519
umar244 <umarsuhail@brandver.se>
2021-08-12 12:07:40 +0500

    second commit

--- lib/AddEntry.dart
+++ lib/AddEntry.dart
@@ -5,7 +5,40 @@ class EntryForm extends StatefulWidget {
 }
 
 class _EntryForm extends State<EntryForm> {
+  List<DropdownMenuItem> dropDown = [];
+  String entryType = 'Cash In';
+
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(),
+            TextField(),
+            DropdownButton<String>(
+              value: entryType,
+              icon: const Icon(Icons.arrow_downward),
+              iconSize: 24,
+              elevation: 16,
+              style: const TextStyle(color: Colors.deepPurple),
+              underline: Container(
+                height: 2,
+                color: Colors.deepPurpleAccent,
+              ),
+              onChanged: (String? newValue) {
+                setState(() {
+                  entryType = newValue!;
+                });
+              },
+              items: <String>['One', 'Two', 'Free', 'Four']
+                  .map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+            )
+          ],
+        ));
   }
 }
